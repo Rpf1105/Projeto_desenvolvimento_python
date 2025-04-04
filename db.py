77eb37d9c07fbd4bdca857b46068e7a1f3b74824
Rpf1105 <rogeriofilho1105@gmail.com>
2025-04-04 15:05:00 -0300

    refatoracao do banco de dados e adionadas funcoes por terminal

--- db.py
+++ db.py
@@ -1,21 +1,62 @@
 import sqlite3
 import sqlite3 as conector
+from fileinput import filename
 
 
-def defineDb(conexao5):
-    cursor = conexao.cursor()
-    query = '''CREATE TABLE IF NOT EXISTS Pessoa(
-	nome varchar(255) NOT NULL,
-	cpf varchar(255) NOT NULL,
-	nascimento date NOT NULL,
-	oculos boolean NOT NULL,
-	PRIMARY KEY(cpf)
+def defineDb(conexao):
+    foreignkey = '''PRAGMA foreign_keys = ON;'''
+
+    delete = '''DROP TABLE IF EXISTS Inscrição'''
+    query1 = '''CREATE TABLE IF NOT EXISTS Aluno(
+	    nome varchar(255) NOT NULL,
+	    matricula varchar(255) NOT NULL,
+	    PRIMARY KEY(matricula)
+    )'''
+    query2 = '''CREATE TABLE IF NOT EXISTS Disciplina(
+    	nome varchar(255) NOT NULL,
+    	codigo varchar(255) NOT NULL,
+    	PRIMARY KEY(codigo)
+    )'''
+    query3 = '''CREATE TABLE IF NOT EXISTS Inscrição(
+        aluno varchar(255),
+        disciplina varchar(255),
+    	Ano INT NOT NULL,
+    	Semestre INT NOT NULL,
+    	Sim1 Real,
+    	Sim2 Real,
+    	Av Real,
+    	Avs Real,
+    	NF Real,
+    	FOREIGN KEY (aluno) REFERENCES Aluno(matricula),
+    	FOREIGN KEY (disciplina) REFERENCES Disciplina(codigo),
+    	PRIMARY KEY (aluno, disciplina)
     )'''
     try:
-        queryTest(cursor, query)
+        #queryTest(conexao, delete)
+        queryTest(conexao, foreignkey)
+        queryTest(conexao, query1)
+        queryTest(conexao, query2)
+        queryTest(conexao, query3)
+
     except ValueError as e:
         print(f"Erro: {e}")
-    cursor.close()
+
+class Aluno:
+    def __init__(self, nome, matricula):
+        self.nome = nome
+        self.matricula = matricula
+
+class Disciplina:
+    def __init__(self, nome, codigo):
+        self.nome = nome
+        self.codigo = codigo
+
+class Inscricao:
+    def __init__(self, aluno, disciplina, ano, semestre):
+        self.aluno = aluno
+        self.disciplina = disciplina
+        self.ano = ano
+        self.semestre = semestre
 
 def connectDb():
     try:
@@ -25,51 +66,73 @@ def connectDb():
         print("Erro de banco de dados", err)
         connectDb()
 
-def insertTable(conexao, table):
-    cursor = conexao.cursor()
-    query = f'''
-        INSERT INTO {table} (cpf, nome, nascimento, oculos) VALUES
-        (
-        '{input("Digite o cpf:")}', 
-        '{input("Digite o nome:")}', 
-        '{input("Digite a data de nascimento em YYYY/MM/DD\n")}',
-        {input("Digite se usa oculos 0-nao, 1-sim:")}
-        )
-        '''
+def queryExec(conexao, op, table, obj):
+    query = getSqlQuery(op, table)
+    if query == '0':
+        return 0
     try:
-        queryTest(cursor, query)
+        queryTest(conexao, query, vars(obj))
+    except TypeError:
+        queryTest(conexao, query, obj)
     except ValueError as e:
         print(f"Erro: {e}")
-    finally:
-        cursor.close()
 
-
-def queryAll(conexao):
-    cursor = conexao.cursor()
-    query = '''SELECT * FROM Pessoa'''
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-    cursor.close()
-
-def queryTest(cursor, query):
+def getSqlQuery(op, key):
     try:
-        print(query)
-        cursor.execute(query)
+        querylist = {
+            '1':{ #insert
+            '1': '''INSERT INTO Aluno VALUES(:nome, :matricula);''',
+            '2': '''INSERT INTO Disciplina VALUES(:nome, :codigo);''',
+            '3': '''INSERT INTO Inscrição(aluno,disciplina,Ano, Semestre) VALUES(:aluno, :disciplina, :ano, :semestre)'''
+            },
+            '2':{ #update
+            '1': '''UPDATE Aluno SET nome = :nome WHERE matricula = :matricula''',
+            '2': '''UPDATE Disciplina SET nome = :nome WHERE codigo = :codigo''',
+            '3': '''UPDATE Inscrição SET :av = :nota WHERE disciplina = :disciplina AND aluno = :aluno'''
+            },
+            '3':{ #delete
+            '1': '''DELETE FROM Aluno WHERE matricula = :matricula''',
+            '2': '''DELETE FROM Disciplina WHERE codigo = :codigo''',
+            '3': '''DELETE FROM Inscrição WHERE disciplina = :disciplina AND aluno = :aluno'''
+            }
+        }
+        return querylist[op][key]
+    except KeyError:
+        return '0'
+
+def queryTest(conexao, query, params):
+    cursor = conexao.cursor()
+    try:
+        cursor.execute(query, params)
         conexao.commit()
     except sqlite3.OperationalError:
-        raise ValueError("Erro de operacao")
-    #except sqlite3.IntegrityError:
-        #raise ValueError("Erro de integridade")
-
+        cursor.executemany(query, params)
+    except sqlite3.IntegrityError:
+        raise ValueError("Erro de integridade")
+    finally:
+        cursor.close()
 
 def closeDb(conexao):
     if conexao:
         conexao.close()
 
-conexao = connectDb()
-defineDb(conexao)
-#insertTable(conexao, "Pessoa")
-queryAll(conexao)
-closeDb(conexao)
+def queryExecMany(con, query):
+    lista = []
+    filepath = "D:\\faculdade\\python\\test\\lista_alunos"
+    file = open(filepath, "r")
+    for line in file:
+        line = line.strip()
+        line = line.split(",")
+        lista.append(tuple(line))
+    print(lista)
+    data = [
+        ('Jane', '1'),
+        ('Joe', '2'),
+        ('John', '3'),
+    ]
+    queryTest(con, query, data)
+
+#quer = '''INSERT INTO Inscrição(aluno,disciplina,Ano, Semestre) VALUES(?, ?, 2025, 1)'''
+queryExecMany(connectDb(),"INSERT INTO Aluno (nome, matricula) VALUES (?, ?)")
+
+
--- main.py
+++ main.py
@@ -1,26 +1,55 @@
 #Rogerio Perso Filho
 
-
 import os
+from operator import truediv
+
+import db
 import notaInput
-import notaOpen
 
 pasta = notaInput.pasta_alunos
-
 if not os.path.exists(pasta):
     os.mkdir(pasta)
+
 def main():
+    con = db.connectDb()
     op = ""
-    while op != "sair":
+    while True:
         print("Bem vindo, digite qual operacao voce deseja realizar:\n"
           "Sair: Sair do programa\n"
-          "1: Registrar a nota de um aluno\n"
-          "2: Ler todas as notas armazenadas\n")
+          "1: Registrar uma entrada ao DB\n"
+          "2: Modificar uma entrada no DB\n"
+          "3: Deletar uma entrada do DB\n"
+        )
         op = input().lower()
-        if op == "1":
-            notaInput.registrar(notaInput.getmatricula(), notaInput.getnome(), notaInput.getnota())
-        elif op == "2":
-            notaOpen.menuNota()
+        if op =="sair":
+            return 0
+        print("Qual tabela voce deseja realizar essa operação:\n"
+              "1-Alunos\n"
+              "2-Disciplinas\n"
+              "3-Inscrições\n"
+              )
+        table = input()
+        if op == "1" or op == "2":
+            if table == "1":
+                db.queryExec(con, op, table, db.Aluno(input("Digite o nome do aluno: "), input("Digite a matricula do aluno")))
+            elif table == "2":
+                db.queryExec(con, op, table, db.Disciplina(input("Digite o nome da Disciplina: "), "ARA0000"))
+            elif table == "3":
+                db.queryExec(con, op, table, db.Inscricao(input("Digite a matricula do aluno: "), input("Digite o codigo da disciplina:"), input("Digite o ano:"), input("Digite o semestre")))
+        else:
+            if table == "1":
+                db.queryExec(con, op, table, {
+                    "matricula": input("Digite a matricula do aluno")
+                })
+            elif table == "2":
+                db.queryExec(con, op, table,{
+                    "codigo":input("Digite o codigo da disciplina"),
+                })
+            elif table == "3":
+                db.queryExec(con, op, table, {
+                    "disciplina":input("Digite o codigo da disciplina"),
+                    "aluno": input("Digite a matricula do aluno")
+                })
 
 main()
 
